063e75f0802f9adb25e6ccf3f06947a1fcde9c24
Igor Ivanov <iia02@yandex.ru>
2026-09-18 21:57:43 +0300

    Finished step 5

--- gendiff.js
+++ gendiff.js
@@ -12,6 +12,7 @@ program
     .option('-f, --format [type]', 'output format')
     .arguments('<filepath1> <filepath2>')
     .action((filepath1, filepath2) => {
-      genDiff(path.resolve(filepath1), path.resolve(filepath2));
+      const result = genDiff(path.resolve(filepath1), path.resolve(filepath2));
+      console.log(result);
 });
 program.parse();    
